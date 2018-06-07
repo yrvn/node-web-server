f4fc933b1669cffaff760ea7e3c5bba652955ae2
Yuri Alvarez <yurialvarez1984@gmail.com>
2018-06-06 21:11:26 -0300

    Added projects

--- server.js
+++ server.js
@@ -53,6 +53,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'Portfolio'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to fullfil the request'
